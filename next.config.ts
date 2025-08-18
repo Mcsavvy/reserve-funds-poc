6db71786175482794925780eb83fd9124efadf42
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-08-18 16:25:13 +0100

    chore: update Next.js configuration to ignore ESLint and TypeScript errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // don't lint on build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // don't check types on build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
